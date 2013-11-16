e94246d1dbeab575285fc83a2c5073b81738abfe
YuTinCH <yuntingcheng@gmail.com>
2013-11-16 22:21:28 +0800

    有關Qt亂碼的程式碼
    http://chenglearning.blogspot.tw/2013/11/qt51.html

---
+++ Qt解決亂碼問題/firstqtproject/mainwindow.cpp
@@ -0,0 +1,25 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QTextCodec>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->comboBox->setEnabled(false);
+    QStringList cityname;
+    cityname<<QStringLiteral("基隆市")<<QStringLiteral("台北市")<<QStringLiteral("新北市");
+    //cityname.
+    ui->comboBox->addItems(cityname);
+
+}
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::on_checkBox_clicked(bool checked)
+{
+    if(checked)  ui->comboBox->setEnabled(true);
+    else ui->comboBox->setEnabled(false);
+}
