acfa2ef5f2ebc3f6d03be59149995636df7290b7
YuTinCH <yuntingcheng@gmail.com>
2013-11-25 00:18:20 +0800

    Qt入門篇(3):連結SQLITE資料庫
    http://chenglearning.blogspot.tw/search/label/QTBasic

---
+++ Qt Sqlite/firstqtproject/DBConnection.cpp
@@ -0,0 +1,5 @@
+#include "DBConnection.h"
+
+DBConnection::DBConnection()
+{
+}
---
+++ Qt Sqlite/firstqtproject/mainwindow.cpp
@@ -0,0 +1,42 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QTextCodec>
+#include <QtSql/QtSql>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->comboBox->setEnabled(false);
+    QStringList cityname;
+    if(DBConnection(cityname))
+        ui->comboBox->addItems(cityname);
+
+}
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::on_checkBox_clicked(bool checked)
+{
+    if(checked)  ui->comboBox->setEnabled(true);
+    else ui->comboBox->setEnabled(false);
+}
+bool MainWindow::DBConnection(QStringList &citylist)
+{
+    QSqlDatabase db;
+    db=QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("Taiwan");
+    if(db.open())
+    {
+        QSqlQuery query("Select City_cname from Taiwan_City");
+        while(query.next())
+        {
+            citylist.append(query.value(0).toString().trimmed());
+        }
+        qDebug()<<"Success";
+         return true;
+    }
+    else return false;
+}
